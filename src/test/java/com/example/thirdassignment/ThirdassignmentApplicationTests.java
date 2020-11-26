1c45c156ccb4d1a38d88c22b152e26c1bf1283de
Nicholas Bernard <nbernard@albany.edu>
2020-11-26 05:01:34 -0500

    Initial commit

---
+++ src/test/java/com/example/thirdassignment/ThirdassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thirdassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdassignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
